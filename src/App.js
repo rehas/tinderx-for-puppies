0df7cd474452c2df05eead33497753d7f2aea210
“TOM <YOUR@EMAIL.com>
2018-07-24 13:45:44 +0200

    pages added to routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import {Route} from 'react-router-dom'
 import SelectorContainer from './containers/SelectorContainer';
+import UserTypePageContainer from './containers/UserTypePageContainer'
 
 
 class App extends Component {
@@ -13,10 +14,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <SelectorContainer/>
+        <main>
+          <Route exact path = '/:id/selector' component ={SelectorContainer}  />
+          <Route exact path = '/user-types' component ={UserTypePageContainer}  />
+        </main>
       </div>
     );
   }
