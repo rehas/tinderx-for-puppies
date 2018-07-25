fba35eb4461cd8b9c8f442be407ce42b0e96f119
NoemiMolnar <noemi.m.molnar@gmail.com>
2018-07-25 10:25:24 +0200

    refactoring the yes, please and no, thanks actions' redusers

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,8 +1,26 @@
 import users from '../users/users-hardcoded'
+import {YES_PLEASE, NO_THANKS, } from '../actions/user-actions'
+import {SHOW_NEW_PROFILE} from '../actions/profile-actions'
+
+
 
 const initialState = users
 
 export default (state = initialState, action = {}) => {
-  
-  return state
+  switch (action) {
+    case SHOW_NEW_PROFILE:
+    const currentUser = users.filter(user => action.payload.userId === user.Id)[0]
+      return [...state,
+        {...currentUser} ]
+
+        case YES_PLEASE:
+        return [...state][currentUser].Yes = state.Yes.concat(action.payload)
+      
+      case NO_THANKS:
+        return [...state][currentUser].No = state.No.concat(action.payload)
+
+    default:
+      return state
+  }
+
 }
