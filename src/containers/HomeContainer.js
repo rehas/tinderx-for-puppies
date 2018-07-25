15a7110c08ec5021652762acf8d9b99ab25529db
“TOM <YOUR@EMAIL.com>
2018-07-25 15:59:12 +0200

    fixed conflicts in app

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -15,7 +15,7 @@ class HomeContainer extends PureComponent {
     const validatePassword = this.props.users.filter(user => user.Password === this.state.userPassword)
     if (validateEmail.length === 1 && validatePassword.length === 1) {
 
-      this.props.setCurrentUser(parseInt(validateEmail[0].Id, 10))
+      this.props.setCurrentUser(parseInt(validateEmail[0].Id, 10));
       }
     else {
       console.log("error")
