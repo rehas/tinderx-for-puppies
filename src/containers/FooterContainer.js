ac607d38a5de54a7d508e9ee4dea4f3d1e66be77
RehaS <beratson@gmail.com>
2018-07-26 13:12:16 +0200

    matches button hide and show depending on location, favicon app name changed

--- src/containers/FooterContainer.js
+++ src/containers/FooterContainer.js
@@ -4,10 +4,16 @@ import { Link } from 'react-router-dom';
 
 export default class FooterContainer extends PureComponent{
   render(){
+    console.log("footerContainer")
+    console.log(this.props.location.pathname)
+    console.log(this.props.location.pathname === '/')
+
+    const matchButtonClass = (this.props.location.pathname === '/' || this.props.location.pathname.includes('matches')) 
+      ? 'd-none' : ''
     return (
       <div className="card-footer page-footer"> 
       I'm the footer
-      <Link to={ `/${this.props.currentUser}/matches` }><button>My matches</button></Link>
+      <Link to={ `/${this.props.currentUser}/matches` }><button className={matchButtonClass}>My matches</button></Link>
       
       </div>
     )
